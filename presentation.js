6d1c6a1a1e5eb0b0588db2edca8c3b8c2a515d6c
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-09-19 11:41:28 +0200

    tp3 terminé

--- presentation.js
+++ presentation.js
@@ -1,71 +1,76 @@
 // récupération du module `readline`
-var readline = require('readline');
-var service = require('./service.js');
+const readline = require('readline');
+const { Service } = require('./service.js');
+
+const service = new Service();
 
 // création d'un objet `rl` permettant de récupérer la saisie utilisateur
-var rl = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-var start = function () {
+const start = () => {
 
-    rl.question('Veuillez saisir votre email\n', function (saisieMail) {
-        rl.question('Veuillez saisir votre mot de passe\n', function (saisieMdp) {
+    rl.question('Veuillez saisir votre email\n', (saisieMail) => {
+        rl.question('Veuillez saisir votre mot de passe\n', (saisieMdp) => {
 
-            service.auth(saisieMail, saisieMdp, function (result) {
+            service.auth(saisieMail, saisieMdp)
+                .then(() => {
+                    console.log('Vous êtes bien connecté');
 
-                var menu = '1. Rechercher un collègue par nom \n2. Créer un collègue \n99. Sortir \n';
-                // récupération de la saisie utilisateur
-                rl.question(menu, function (saisie) {
+                    const menuFonction = () => {
+                        const menu = '1. Rechercher un collègue par nom \n2. Créer un collègue \n99. Sortir \n';
+                        // récupération de la saisie utilisateur
+                        rl.question(menu, (saisie) => {
 
-                    if (saisie === '1') {
-                        rl.question('Veuillez saisir un nom :\n', function (saisieNom) {
+                            if (saisie === '1') {
+                                rl.question('Veuillez saisir un nom :\n', (saisieNom) => {
 
-                            service.findColl(saisieNom, function (resultNom) {
-                                var tabMatricule = resultNom;
-
-                                for (i = 0; i < tabMatricule.length; i++) {
-                                    service.findByMat(tabMatricule[i], function (resultMat) {
-                                        var infoColl = resultMat;
-                                        console.log(infoColl);
-                                    }, function(err){
-                                        console.log(err)
-                                    });
-                                } 
-                            });
-                        });
-                        start();
-                    }
-                    if (saisie === '2') {
-                        rl.question('Nom : \n', function(nom){
-                            rl.question('Prenoms : \n', function(prenoms){
-                                rl.question('Email : \n', function(email){
-                                    rl.question('Date de naissance : \n', function(dateDeNaissance){
-                                        rl.question('Url de votre photo : \n', function(photoUrl){
-                                            rl.question('Mot de passe : \n', function(motDePasse){
-                                                service.createColl(nom,prenoms,email,dateDeNaissance,photoUrl,motDePasse, function(newColl){
-                                                    console.log(newColl);
-                                                }, function(messageErreur){
-                                                    console.log(messageErreur);
+                                    service.findColl(saisieNom)
+                                        .then((tabColl) => {
+                                            tabColl.forEach(coll => console.log(`${coll.nom} ${coll.prenoms} (${coll.dateDeNaissance})`));
+                                            menuFonction();
+                                        });
+                                });
+                            }
+                            if (saisie === '2') {
+                                rl.question('Nom : \n', (nom) => {
+                                    rl.question('Prenoms : \n', (prenoms) => {
+                                        rl.question('Email : \n', (email) => {
+                                            rl.question('Date de naissance : \n', (dateDeNaissance) => {
+                                                rl.question('Url de votre photo : \n', (photoUrl) => {
+                                                    rl.question('Mot de passe : \n', (motDePasse) => {
+                                                        service.createColl(nom, prenoms, email, dateDeNaissance, photoUrl, motDePasse)
+                                                            .then((newColl) => {
+                                                                console.log(newColl);
+                                                                menuFonction();
+                                                            }, (err) => {
+                                                                console.log("Erreur de saisie lors de la création du collègue");
+                                                                menuFonction();
+                                                            });
+                                                    });
                                                 });
                                             });
                                         });
                                     });
                                 });
-                            });
+                            }
+                            if (saisie === '99') {
+                                console.log('A+');
+                                rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
+                            }
+
                         });
-                    }
-                    if (saisie === '99') {
-                        console.log('A+');
-                        rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
+
                     }
 
-                });
-            },
-                function (err) {
-                    console.log('Erreur d\'identifiant');
-                });
+                    menuFonction();
+                },
+                    (err) => {
+                        console.log('Erreur d\'identifiant');
+                        start();
+                    });
         })
     })
 }
--- service.js
+++ service.js
@@ -1,92 +1,35 @@
-   // création d'une requête avec activation de suivi de Cookies.
-   var request = require('request').defaults({ jar: true });
+// création d'une requête avec activation de suivi de Cookies.
+const request = require('request-promise-native').defaults({ jar: true });
 
-var auth = function (email, motDePasse, callBackFn, errorFn) {
+class Service {
+    constructor() { }
 
- 
+    auth(email, motDePasse) {
+        return request('http://localhost:8080/auth',
+            {
+                method: 'POST',
+                json: true,
+                body: { email, motDePasse }
+            });
+    }
 
-    request('http://localhost:8080/auth',
-        {
-            method: 'POST',
-            json: true,
-            body: {
-                email: email,
-                motDePasse: motDePasse
-            }
-        },
-        function (err, res, result) {
-            // traiter résultat de la requête
-            if (res.statusCode === 200) {
-                callBackFn(result);
-            } else {
-                errorFn('Erreur');
-            }
-        });
-}
-
-var createColl = function (nom, prenoms, email, dateDeNaissance, photoUrl, motDePasse, callBackFn, errorFn) {
-   
-    request('http://localhost:8080/collegues',{
-        method: 'POST',
-        json: true,
-        body: {
-            nom: nom,
-            prenoms: prenoms,
-            email: email,
-            dateDeNaissance: dateDeNaissance,
-            photoUrl: photoUrl,
-            motDePasse: motDePasse
-        }
-    },
-    function (err, res, result) {
-        if(res.statusCode === 200) {
-            callBackFn(result);
-        } else {
-            errorFn('Erreur lors de la création d\'un collègue');
-        }
-    });
-}
-
-var findColl = function (nom, callBackFn, errorFn) {
+    createColl(nom, prenoms, email, dateDeNaissance, photoUrl, motDePasse) {
+        return request('http://localhost:8080/collegues',
+            {
+                method: 'POST',
+                json: true,
+                body: { nom, prenoms, email, dateDeNaissance, photoUrl, motDePasse }
+            });
+    }
     
-    request('http://localhost:8080/collegues?nom=' + nom,{
-        method: 'GET',
-        json: true,
-        body: {
-
+        findColl(nom) {
+            return request(`http://localhost:8080/collegues?nom=${nom}`, { json: true })
+                .then(tabMatricules => {
+                    return Promise.all(tabMatricules
+                        .map(matricule => `http://localhost:8080/collegues/${matricule}`)
+                        .map(url => request(url, { json: true })));
+                });
         }
-    },
-    function (err, res, result) {
-        if (res.statusCode === 200) {
-            callBackFn(result);
-        } else {
-            errorFn('Aucun collègue ne possède ce nom');
-        }
-    });
-}
+    }
 
-var findByMat = function (matricule, callBackFn, errorFn) {
- 
-
-    request('http://localhost:8080/collegues/' + matricule,{
-        method: 'GET',
-        json: true,
-        body: {
-
-        }
-    },
-    function (err, res, result) {
-        if (res.statusCode === 200) {
-            callBackFn(result.nom + ' ' + result.prenoms + ' (' + result.dateDeNaissance + ')');
-        } else {
-            errorFn('Aucun collègue ne possède ce matricule');
-        }
-    });
-}
-
-module.exports = {
-    auth: auth,
-    findColl: findColl,
-    findByMat: findByMat,
-    createColl: createColl
-}
+module.exports = { Service }
